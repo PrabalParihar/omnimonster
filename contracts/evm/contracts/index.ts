a67ceabe310f675893435bdbcc70d2a20f525040
PrabalParihar <prabalparihar95@gmail.com>
2025-08-03 02:53:10 +0530

    feat: add contract index and exports
    - Add index file for contract exports
    - Better contract organization
    - Support for multiple contract versions
    - Easier contract imports

---
+++ contracts/evm/contracts/index.ts
@@ -0,0 +1,6 @@
+export { SwapSageHTLCv2 } from './SwapSageHTLCv2.sol';
+export { SwapSageHTLCForwarder } from './SwapSageHTLCForwarder.sol';
+export { MinimalForwarder } from './MinimalForwarder.sol';
+export { MockERC20 } from './MockERC20.sol';
+export { FusionHTLC } from './FusionHTLC.sol';
+export { FusionGasRelayer } from './FusionGasRelayer.sol';
